d50448e8de5764dc3f9c51f7e3bbe35d854586cc
vitthal-07 <vitthalbiradar50@gmail.com>
2024-03-08 22:50:53 +0530

    Final Upgradations

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import TodoApplication from "./Components/TodoApplication";
 import ApplicationHeader from "./Components/ApplicationHeader";
 import "./App.css";
 import { TodoList } from "./Components/TodoList";
 import BlankState from "./Components/BlankState";
-import { TodoItems } from "./store/todo-items";
+import TodoItemContextProvider from "./store/todo-items";
 
 function App() {
-	const [tasks, setTasks] = useState([]);
-
-	// const handleAddTask = (task, dueDate) => {
-	// 	if (task !== "" && dueDate !== "") {
-	// 		const newTask = { task, dueDate };
-	// 		setTasks([...tasks, newTask]);
-	// 	}`
-	// };
-
-	const addNewItem = (task, dueDate) => {
-		if (task !== "" && dueDate !== "") {
-			const newTask = { task, dueDate };
-			setTasks([...tasks, newTask]);
-		}
-	};
-
-	const deleteItem = (index) => {
-		const updatedTasks = tasks.filter((_, i) => i !== index);
-		setTasks(updatedTasks);
-	};
-
 	return (
 		<>
-			<TodoItems.Provider value={{ tasks, addNewItem, deleteItem }}>
+			<TodoItemContextProvider>
 				<ApplicationHeader />
 				<TodoApplication />
 				<BlankState />
 				<TodoList />
-			</TodoItems.Provider>
+			</TodoItemContextProvider>
 		</>
 	);
 }
--- src/store/todo-items.jsx
+++ src/store/todo-items.jsx
@@ -1,7 +1,52 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const TodoItems = createContext({
 	tasks: [],
 	addNewItem: () => {},
 	deleteItem: () => {},
 });
+
+const todoItemReducer = (currState, action) => {
+	let newState = currState;
+	if (action.type === "ADD_ITEM") {
+		if (action.payload.task !== "" && action.payload.dueDate !== "") {
+			newState = [
+				...newState,
+				{ task: action.payload.task, dueDate: action.payload.dueDate },
+			];
+		}
+	} else if (action.type === "DELETE_ITEM") {
+		newState = currState.filter((_, i) => i !== action.payload.index);
+	}
+	return newState;
+};
+
+function TodoItemContextProvider({ children }) {
+	const [tasks, dispatchTasks] = useReducer(todoItemReducer, []);
+	const addNewItem = (task, dueDate) => {
+		const addItem = {
+			type: "ADD_ITEM",
+			payload: {
+				task,
+				dueDate,
+			},
+		};
+		dispatchTasks(addItem);
+	};
+
+	const deleteItem = (index) => {
+		const addItem = {
+			type: "DELETE_ITEM",
+			payload: { index },
+		};
+		dispatchTasks(addItem);
+	};
+
+	return (
+		<TodoItems.Provider value={{ tasks, addNewItem, deleteItem }}>
+			{children}
+		</TodoItems.Provider>
+	);
+}
+
+export default TodoItemContextProvider;
